5383c248277e105ebd65742b6982729214c2170e
stkchan <stkza007hod@hotmail.com>
2024-10-19 11:23:30 +0700

    checkpoint-1

--- first.py
+++ first.py
@@ -1,1 +1,19 @@
-print("Hello Mundo")
+import sqlite3
+
+conn = sqlite3.connect('expense.sqlite3')
+c = conn.cursor()
+
+c.execute("""CREATE TABLE IF NOT EXISTS expense (
+                ID      INTEGER PRIMARY KEY AUTOINCREMENT,  
+                title   TEXT,
+                price   REAL,
+                others  TEXT,
+                timestamp   TEXT )
+          """)
+
+
+#Insert Data
+with conn:
+    command = 'INSERT INTO expense VALUES (?, ?, ?, ?, ?)' #SQL
+    c.execute(command, (None, '7-11' , 43, 'tofusan_soymilk_chocolate-cacao', '2024-10-19 11:11:11'))
+conn.commit() #Save data to database
